6341feccb0286648988a0d0ca4ce30ec34cca096
hukuanmin <kuanmin.hkm@alibaba-inc.com>
2016-06-13 11:02:22 +0800

    save

--- skeletons/javascriptAbstract.js
+++ skeletons/javascriptAbstract.js
@@ -9,6 +9,10 @@
     throwAbstractFunctionError: function(functionName) {
       throw new Error('Abstract function not implemented, ' + functionName);
     },
+    create: function(implClass) {
+      var tmpArguments = Array.prototype.slice.call(arguments, 1);
+      return new (Function.prototype.bind.apply(implClass, tmpArguments));
+    },
     exampleFunction: function() {
       this.throwAbstractFunctionError('exampleFunction');
     }
